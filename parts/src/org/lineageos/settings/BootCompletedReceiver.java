0b2170d931c977d7708d68f87f42d307847fc497
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2022-08-29 18:15:59 +0000

    sm8250-common: parts: Implement HBM & Auto HBM
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: EmanuelCN <emanuelgithub@gmail.com>
    Co-Authored-By: Max Weninger <max.weninger@gmail.com>
    Co-Authored-By: AnierinB <anierin@evolution-x.org>
    Co-Authored-By: EmanuelCN <emanuelgithub@gmail.com>
    Co-Authored-By: Yohan Black <deibynovoa3@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -32,7 +32,6 @@ import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.utils.FileUtils;
-
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
@@ -58,5 +57,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
         FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
+        FileUtils.enableService(context);
     }
 }
